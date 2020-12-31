9d62804ef1795e73825fc3d5ff2adbf620a94a89
Matteo Semenzato <mattew8898@gmail.com>
2020-12-31 20:06:41 +0100

    Update limine

--- src-stivale2/main-stivale2.c
+++ src-stivale2/main-stivale2.c
@@ -22,7 +22,7 @@
 #define VGA_WHITE        15
 
 static uint8_t stack[4096] = {0};
-void stivale2_main(struct stivale2_struct *info);
+void _start(struct stivale2_struct *info);
 
 struct stivale2_header_tag_smp smp_request = {
     .tag = {
@@ -34,13 +34,13 @@ struct stivale2_header_tag_smp smp_request = {
 
 __attribute__((section(".stivale2hdr"), used))
 struct stivale2_header header2 = {
-    .entry_point = (uint64_t)stivale2_main,
+    .entry_point = (uint64_t)_start,
     .stack       = (uintptr_t)stack + sizeof(stack),
     .flags       = 0,
     .tags        = (uint64_t)&smp_request
 };
 
-void stivale2_main(struct stivale2_struct *info) {
+void _start(struct stivale2_struct *info) {
     volatile uint16_t *vga_buffer = (uint16_t*)VGA_ADDRESS;
     vga_buffer[0] = VGA_COLOR('h', VGA_GREEN);
     vga_buffer[1] = VGA_COLOR('e', VGA_GREEN);
